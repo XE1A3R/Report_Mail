cdb939fcccc2fcd6a2e210d7cf0678c84f7e6d24
Александр Люстик <erpresents@gmail.com>
2019-02-20 02:52:42 +0300

    Добавьте файлы проекта.

---
+++ Report_Mail/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Report_Mail
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// backgroundWorker1
+			// 
+			this.backgroundWorker1.WorkerReportsProgress = true;
+			this.backgroundWorker1.WorkerSupportsCancellation = true;
+			this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+			this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+			this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.progressBar1.ForeColor = System.Drawing.Color.Red;
+			this.progressBar1.Location = new System.Drawing.Point(12, 58);
+			this.progressBar1.MarqueeAnimationSpeed = 1;
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(210, 23);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.progressBar1.TabIndex = 0;
+			this.progressBar1.Visible = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(43, 62);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(10, 10);
+			this.dataGridView1.TabIndex = 1;
+			this.dataGridView1.Visible = false;
+			// 
+			// backgroundWorker2
+			// 
+			this.backgroundWorker2.WorkerReportsProgress = true;
+			this.backgroundWorker2.WorkerSupportsCancellation = true;
+			this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker2_DoWork);
+			this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+			this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(146, 62);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(10, 10);
+			this.dataGridView2.TabIndex = 2;
+			this.dataGridView2.Visible = false;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 42);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "label1";
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = global::Report_Mail.Properties.Settings.Default.Timer;
+			this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.BalloonTipTitle = "Завершение программы";
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(234, 93);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.dataGridView2);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker2;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+	}
+}
+
---
+++ Report_Mail/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace Report_Mail
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			label1.Text = "";
+			//Thread Run = new Thread(Worker_1);
+			//Run.Start();
+			Worker_1();
+		}
+
+		void Worker_1()
+		{
+			progressBar1.Visible = true;
+			backgroundWorker1.RunWorkerAsync();
+		}
+
+		void Worker_2()
+		{
+			backgroundWorker2.RunWorkerAsync();
+		}
+
+		private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			this.Invoke(new Action(Report_1));
+		}
+
+		private void BackgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+		{
+
+			//this.Invoke(new Action(Report_2));
+		}
+		private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			progressBar1.Value = e.ProgressPercentage;
+		}
+
+		private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			Invoke(new Action(Sleep_Exit));
+		}
+		void Report_1()
+		{
+			OdbcConnection cnS11 = new OdbcConnection(Properties.Settings.Default.S11);
+			OdbcDataAdapter adapter1 = new OdbcDataAdapter("SELECT CONCAT('с ', DATE_FORMAT(DATE_sub(CURDATE(), INTERVAL 18 HOUR), '%d.%m.%Y %H:%i'), ' по ', DATE_FORMAT(DATE_ADD(CURDATE(), INTERVAL 6 HOUR), '%d.%m.%Y %H:%i'))", cnS11);
+			DataTable table1 = new DataTable();
+			adapter1.Fill(table1);
+			dataGridView2.DataSource = table1;
+			var xls = dataGridView2[0, 0].Value.ToString();
+			label1.Text = "Выполняется процедура...";
+			OdbcDataAdapter adapter = new OdbcDataAdapter("CALL procStatementForPastDay", cnS11);
+			DataTable table = new DataTable();
+			adapter.Fill(table);
+			dataGridView1.DataSource = table;
+			Console.WriteLine(xls);
+			progressBar1.Maximum = dataGridView1.RowCount + dataGridView1.ColumnCount;
+			backgroundWorker1.ReportProgress(dataGridView1.RowCount + dataGridView1.ColumnCount);
+			backgroundWorker1.ReportProgress(0);
+			label1.Text = "Выгрузка в EXCEL...";
+			Excel.Application xlApp;
+			Excel.Workbook xlWorkBook;
+			Excel.Worksheet xlWorkSheet;
+			object misValue = System.Reflection.Missing.Value;
+
+			Int16 i, j;
+			xlApp = new Excel.Application();
+			xlWorkBook = xlApp.Workbooks.Add(misValue);
+			xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+			xlApp.Cells[1, 1] = "Отчет";
+			xlApp.Cells[1, 1].HorizontalAlignment = Excel.Constants.xlRight;
+			xlApp.Cells[1, 2] = xls;
+			xlApp.Cells[2, 1] = "Выписан из Отделения";
+			xlApp.Cells[2, 2] = "Врач";
+			xlApp.Cells[2, 3] = "Выписано";
+			xlApp.Cells[2, 4] = "Без диагноза";
+			xlWorkSheet.Cells[1, 4].EntireRow.Font.Bold = xlWorkSheet.Cells[2, 4].EntireRow.Font.Bold = true;
+
+
+			int h = 1;
+			//xlApp.Cells[1, 1].HorizontalAlignment = Excel.Constants.xlCenter;
+			//xlApp.Cells[1, 2].HorizontalAlignment = Excel.Constants.xlCenter;
+			//xlApp.Cells[1, 3].HorizontalAlignment = Excel.Constants.xlCenter;
+			for (i = 0; i < dataGridView1.RowCount; i++)
+			{
+				h++;				
+				for (j = 0; j < dataGridView1.ColumnCount; j++)
+				{					
+					backgroundWorker1.ReportProgress(i + j);
+					xlApp.Cells[h + 1, 4].Interior.Color = Color.Red;
+					xlWorkSheet.Cells[h + 1, j + 1] = dataGridView1[j, i].Value.ToString();
+				}
+			}
+			backgroundWorker1.ReportProgress(dataGridView1.ColumnCount + dataGridView1.RowCount);
+
+
+			((Excel.Range)xlWorkSheet.Columns[1]).AutoFit();
+			((Excel.Range)xlWorkSheet.Columns[2]).AutoFit();
+			((Excel.Range)xlWorkSheet.Columns[3]).AutoFit();
+			((Excel.Range)xlWorkSheet.Columns[4]).AutoFit();
+
+			xlWorkBook.SaveAs(@"" + Environment.CurrentDirectory + "/Test.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+			xlWorkBook.Close(true, misValue, misValue);
+			xlApp.Quit();
+
+			ReleaseObject(xlWorkSheet);
+			ReleaseObject(xlWorkBook);
+			ReleaseObject(xlApp);
+			try
+			{
+				//	SmtpClient smtp = new SmtpClient("mail.gb15.ru", 25)
+				//	{
+				//		Credentials = new NetworkCredential("robot@gb15.ru", "1oc@1RoBoT")
+				//	};
+				//	MailMessage Message = new MailMessage
+				//	{
+				//		From = new MailAddress("robot@gb15.ru", "Report")
+				//	};
+				//	Message.To.Add(new MailAddress(Properties.Settings.Default.mail_add));
+				//	//Message.CC.Add(new MailAddress(Properties.Settings.Default.mail_cc));
+				//	Message.Subject = xls;
+				//	Message.Body = "Добрый день. \n" +
+				//					"" + Environment.NewLine + "" +
+				//					"Отчет во вложении." +
+				//					"" + Environment.NewLine + "" +
+				//					"" + Environment.NewLine + "" +
+				//					"--\n" +
+				//					"С уважением,\n" +
+				//					"Группа МИС\n" +
+				//					"СПб ГБУЗ 'Городская больница № 15'";
+				//	Message.Attachments.Add(new Attachment("" + Environment.CurrentDirectory + "/Test.xls"));
+				//	try
+				//	{
+
+				//		smtp.Send(Message);
+				//		MessageBox.Show("Отправлено.", "smtp");
+				//	}
+				//	catch (SmtpException)
+				//	{					
+
+				//		MessageBox.Show("Ошибка!", "smtp");
+				//	}
+				//	Message.Attachments.Dispose();
+				//File.Delete(@"" + Environment.CurrentDirectory + "/Test.xls");
+
+			}
+			catch (Exception ex)
+			{
+				//	Log.logger.Error("Error User:{0}, ID:{1}, IP:{2}, Ver:{3} \n" +
+				//		"" + Environment.NewLine + "" +
+				//		"" + Environment.NewLine + "" +
+				//		"     {4}" +
+				//		"     {5}" +
+				//		"" + Environment.NewLine + "" +
+				//		"" + Environment.NewLine + "", Log.username, Data.Person_id, Log.IP_Address, Log.version, отчетToolStripMenuItem, ex.Message);
+				//	if (выводОшибокToolStripMenuItem.Checked == true)
+				//	{
+				//		MessageBox.Show(ex.Message, "Error");
+				//	}
+				//	SmtpClient smtp = new SmtpClient("mail.gb15.ru", 25)
+				//	{
+				//		Credentials = new NetworkCredential("robot@gb15.ru", "1oc@1RoBoT")
+				//	};
+				//	MailMessage Message = new MailMessage
+				//	{
+				//		From = new MailAddress("robot@gb15.ru", "Report")
+				//	};
+				//	Message.To.Add(new MailAddress(Properties.Settings.Default.mail_Error));
+				//	Message.Subject = "Error";
+				//	Message.Body = "Error User: " + Log.username + ", ID: " + Data.Person_id + ", IP: " + Log.IP_Address + ", Ver: " + Log.version + " \n" +
+				//		"     " + отчетToolStripMenuItem + " \n" +
+				//		"     " + ex.Message + "";
+				//	Message.Attachments.Add(new Attachment("" + Environment.CurrentDirectory + "/logs/" + today.ToString("yyyy-MM-dd") + ".log"));
+				//	try
+				//	{
+				//		smtp.Send(Message);
+				//	}
+				//	catch (SmtpException)
+				//	{
+				//		if (выводОшибокToolStripMenuItem.Checked == true)
+				//		{
+				//			MessageBox.Show("Ошибка!", "smtp");
+				//		}
+
+				//	}
+			}
+			//progressBar1.Visible = false;
+		}
+
+		void Report_2()
+		{
+
+		}
+
+		void Sleep_Exit()
+		{
+			timer1.Enabled = true;
+			int times = timer1.Interval;
+			notifyIcon1.BalloonTipText = "Завершение программы начнется через " + Convert.ToString(times / 1000) + " екунд";
+			notifyIcon1.ShowBalloonTip(5000);
+
+		}
+		private void ReleaseObject(object obj)
+		{
+			try
+			{
+				System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+				obj = null;
+			}
+			catch (Exception ex)
+			{
+				obj = null;
+				MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+			}
+			finally
+			{
+				GC.Collect();
+			}
+		}
+
+		private void Timer1_Tick(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+	}
+}

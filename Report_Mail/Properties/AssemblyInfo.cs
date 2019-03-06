d8ae1ac3405e8efb2a81d93ad0bdcc2aa577d813
Александр Люстик <erpresents@gmail.com>
2019-03-06 15:16:21 +0300

    1

--- Report_Mail/Properties/AssemblyInfo.cs
+++ Report_Mail/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // с помощью символа "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.3")]
-[assembly: AssemblyFileVersion("1.0.3.3")]
+[assembly: AssemblyVersion("1.1.3.4")]
+[assembly: AssemblyFileVersion("1.1.3.4")]
